1cca074d34ad617c72da9f83e9c3606e520f99ee
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-02-08 09:54:05 +0300

    Completed assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
+    #setter method
+    def name= (dog_name)
+        #assign the @name instance variable a value that was passed as an arguement
+        @this_dogs_name = dog_name
+    end
 
+    def name
+        @this_dogs_name
+    end
 end
+lassie = Dog.new
+lassie.name = 'Lassie'
+lassie.name
